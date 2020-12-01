19a53839256f9d2a73d1ef8402224dc8c5e8ef35
Roman <romanvistunov@gmail.com>
2020-12-01 23:27:22 +0800

    hosts

--- backend/settings.py
+++ backend/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 
 
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
