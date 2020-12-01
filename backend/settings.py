e7457f5a29d9aa1be45c883dd90a4b91095c56eb
Roman <romanvistunov@gmail.com>
2020-12-01 19:42:53 +0800

    hosts

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9#o%mfm&==xaw+#rubmf7q+!u%5zuk@ba3(*3a53v%&+0@d=l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '94.103.92.111']
 
