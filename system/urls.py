3522d44cb8d993a9fbe06b6a97b8853ecca048e3
Roman <romanvistunov@gmail.com>
2020-12-03 20:06:06 +0800

    static

--- system/urls.py
+++ system/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -11,3 +12,5 @@ urlpatterns = [
     path('catalog/<slug:category_slug>/<slug:subcategory_slug>/', views.product_list_by_subcategory, name='product_list_by_subcategory'),
     path('search/', views.global_search, name='global_search'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
