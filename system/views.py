34a0a2dfcaf5113f5ac46949160ae288cc2cbf9e
Roman <romanvistunov@gmail.com>
2020-12-08 17:46:37 +0800

    order by

--- system/views.py
+++ system/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 def index(request):
     categories = Category.objects.all()
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-created')
     context = {'products': products, 'categories': categories}
     template = 'index.html'
     return render(request, template, context)
@@ -14,7 +14,7 @@ def index(request):
 def global_search(request):
     categories = Category.objects.all()
     query = request.GET.get('text')
-    search_results = Product.objects.filter(Q(name__icontains=query))
+    search_results = Product.objects.filter(Q(name__icontains=query)).order_by('-created')
     context = {'search_results':search_results, 'categories': categories, 'query': query}
     template = 'index.html'
     return render(request, template, context)
@@ -23,10 +23,10 @@ def global_search(request):
 def product_list_by_category(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-created')
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products = products.filter(category=category).order_by('-created')
     template = 'index.html'
     context = {'category': category, 'categories': categories, 'products': products,}
     return render(request, template, context)
@@ -36,11 +36,11 @@ def product_list_by_subcategory(request, category_slug=None, subcategory_slug=No
     category = None
     subcategory = None
     categories = Category.objects.all()
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-created')
     if subcategory_slug:
         category = get_object_or_404(Category, slug=category_slug)
         subcategory = get_object_or_404(Subcategory, slug=subcategory_slug)
-        products = products.filter(category=category, subcategory=subcategory)
+        products = products.filter(category=category, subcategory=subcategory).order_by('-created')
 
     template = 'index.html'
     context = {'category': category, 'categories': categories, 'products': products,}
